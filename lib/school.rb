c13a47d1c36004ef2b8be966c0ca4e9e76d5cf7b
Heloise Taillet <ht2394@columbia.edu>
2019-01-10 02:05:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :school
+  attr_writer :grade
+  attr_reader :add_student, :roster
+
+
+  def initialize(school, roster = {})
+    @school = school
+    @roster= roster
+  end
+
+  def add_student(students, grade)
+    @roster[grade] ||= []
+    students.is_a?(String) ? roster[grade] << students :
+    students.each {|student| @roster[grade] << (student)}
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    self.roster.keys.map { |key| 
+     key = @roster[key].sort
+     binding.pry
+    }
+  end
+  
+end
