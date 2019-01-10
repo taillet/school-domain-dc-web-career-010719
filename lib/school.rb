154384f1819edba7f537fc4f7e2d5c98caa96ef6
Heloise Taillet <ht2394@columbia.edu>
2019-01-10 02:24:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :school
+  attr_writer :grade
+  attr_reader :add_student, :roster
+
+
+  def initialize(school, roster = {})
+    @school = school
+    @roster= roster
+  end
+
+  def add_student(students, grade)
+    @roster[grade] ||= []
+    students.is_a?(String) ? roster[grade] << students :
+    students.each {|student| @roster[grade] << (student)}
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    hash = {}
+    self.roster.each do |grade, students| 
+      hash.store(grade, students.sort!)
+    end
+  end
+end
